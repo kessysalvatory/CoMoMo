f00463d7d367d8168fcf50f7cdbbe16a9682db9e
Salvatory Kessy <51252281+kessysalvatory@users.noreply.github.com>
2021-01-16 04:48:23 +1100

    Update CoMoMo.R
    updated

--- R/CoMoMo.R
+++ R/CoMoMo.R
@@ -38,53 +38,53 @@ fitCoMoMo <- function(models, data = NULL, Dxt = NULL, Ext = NULL, ages.fit = NU
   if (is.null(ages.fit)) ages.fit <- ages
   if (is.null(years.fit)) years.fit <- years
 
-# names of specified models
+  # names of specified models
 
-Specified_Models <- lapply(1:length(models), function(x) names(models[x]))
+  Specified_Models <- lapply(1:length(models), function(x) names(models[x]))
 
-year.eval <- max(years.fit)
+  year.eval <- max(years.fit)
 
-year.max <- year.eval + h
+  year.max <- year.eval + h
 
-# produce predictions for multiple evaluation years.
+  # produce predictions for multiple evaluation years.
 
- if (!is.null(h) && year.max <= max(data$years))
+  if (!is.null(h) && year.max <= max(data$years))
 
-{
+  {
 
-  fitModels <-  lapply(year.eval:year.max, function(yearval) lapply(models, function(x) StMoMo::fit(x, data = data, ages.fit = ages.fit, years.fit = years.fit[1]:yearval)))
+    fitModels <-  lapply(year.eval:year.max, function(yearval) lapply(models, function(x) StMoMo::fit(x, data = data, ages.fit = ages.fit, years.fit = years.fit[1]:yearval)))
 
-  forModels <-  lapply(1:h, function(s) lapply(1:length(models), function(x) forecast(fitModels[[s]][[x]], h = length(h:s))))
+    forModels <-  lapply(1:h, function(s) lapply(1:length(models), function(x) forecast(fitModels[[s]][[x]], h = length(h:s))))
 
-  forRates <-  lapply(1:h, function(i) lapply(forModels[[i]], function(x) {tidyr::pivot_longer(dplyr::bind_cols(ages = ages.fit, as.data.frame(x$rates)),
-                            cols = 2:(length(h:i) + 1), names_to = "year", values_to = "rate")%>%dplyr::mutate(year = as.numeric(year), h = year - min(year) + 1)}))
+    forRates <-  lapply(1:h, function(i) lapply(forModels[[i]], function(x) {tidyr::pivot_longer(dplyr::bind_cols(ages = ages.fit, as.data.frame(x$rates)),
+                                                                                                 cols = 2:(length(h:i) + 1), names_to = "year", values_to = "rate")%>%dplyr::mutate(year = as.numeric(year), h = year - min(year) + 1)}))
 
-  output <- dplyr::bind_rows(lapply(1:h, function(k) lapply(1:length(forRates[[k]]), function(x) dplyr::mutate(forRates[[k]][[x]], model = Specified_Models[[x]]))))
+    output <- dplyr::bind_rows(lapply(1:h, function(k) lapply(1:length(forRates[[k]]), function(x) dplyr::mutate(forRates[[k]][[x]], model = Specified_Models[[x]]))))
 
-  return(output)
+    return(output)
 
- }
+  }
 
-# produce predictions for multiple evaluation years when year.max > max(data$years)
+  # produce predictions for multiple evaluation years when year.max > max(data$years)
 
- else if (!is.null(h) && year.max > max(data$years))
+  else if (!is.null(h) && year.max > max(data$years))
 
- {
+  {
 
-   fitModels <-  lapply(year.eval:max(data$years), function(yearval) lapply(models, function(x) StMoMo::fit(x, data = data, ages.fit = ages.fit, years.fit = years.fit[1]:yearval)))
+    fitModels <-  lapply(year.eval:max(data$years), function(yearval) lapply(models, function(x) StMoMo::fit(x, data = data, ages.fit = ages.fit, years.fit = years.fit[1]:yearval)))
 
-   finalmodel <- c(fitModels, rep(tail(fitModels, n = 1),  year.max - max(data$years)))
+    finalmodel <- c(fitModels, rep(tail(fitModels, n = 1),  year.max - max(data$years)))
 
-   forModels <-  lapply(1:h, function(s) lapply(1:length(models), function(x) forecast(finalmodel[[s]][[x]], h = length(h:s))))
+    forModels <-  lapply(1:h, function(s) lapply(1:length(models), function(x) forecast(finalmodel[[s]][[x]], h = length(h:s))))
 
-   forRates <-  lapply(1:h, function(i) lapply(forModels[[i]], function(x) {tidyr::pivot_longer(dplyr::bind_cols(ages = ages.fit, as.data.frame(x$rates)),
-                          cols = 2:(length(h:i) + 1), names_to = "year", values_to = "rate")%>%dplyr::mutate(year = as.numeric(year), h = year - min(year) + 1)}))
+    forRates <-  lapply(1:h, function(i) lapply(forModels[[i]], function(x) {tidyr::pivot_longer(dplyr::bind_cols(ages = ages.fit, as.data.frame(x$rates)),
+                      cols = 2:(length(h:i) + 1), names_to = "year", values_to = "rate")%>%dplyr::mutate(year = as.numeric(year), h = year - min(year) + 1)}))
 
-   output <- dplyr::bind_rows(lapply(1:h, function(k) lapply(1:length(forRates[[k]]), function(x) dplyr::mutate(forRates[[k]][[x]], model = Specified_Models[[x]]))))
+    output <- dplyr::bind_rows(lapply(1:h, function(k) lapply(1:length(forRates[[k]]), function(x) dplyr::mutate(forRates[[k]][[x]], model = Specified_Models[[x]]))))
 
-   return(output)
+    return(output)
 
- }
+  }
 
 }
 
@@ -123,16 +123,16 @@ CoMoMo.default <- function(models, data = NULL, Dxt = NULL, Ext = NULL, ages.fit
   prediction <- fitCoMoMo(models = models,  data = data, ages.fit = ages.fit, years.fit = years.fit, h = h)
 
   simple <- prediction%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(average = exp(mean(log(rate))))%>%dplyr::ungroup()%>%
-   tidyr::pivot_longer(cols = average, values_to = "rate", names_to = "model")
+    tidyr::pivot_longer(cols = average, values_to = "rate", names_to = "model")
 
-   allForecast_one <- dplyr::bind_rows(prediction,  simple)
+  allForecast_one <- dplyr::bind_rows(prediction,  simple)
 
   # observed rates
 
-   obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),
-                                   cols = 2:(length(data$years) + 1), names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
+  obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),
+                                    cols = 2:(length(data$years) + 1), names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
 
-   allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
+  allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
 
   # compute errors
 
@@ -151,105 +151,105 @@ CoMoMo.weight <- function(models, data = NULL, weight = NULL, Dxt = NULL, Ext = 
 
   prediction <- fitCoMoMo(models = models, data = data, ages.fit = ages.fit, years.fit = years.fit, h = h)
 
-   if (max(weight$weights$h)==h)
+  if (max(weight$weights$h)==h)
 
-   {
-     mortfor <- prediction%>%dplyr::left_join(weight$weights)%>%dplyr::mutate(forrates = log(rate)*model.weights)%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(comb = exp(sum(forrates)))%>%dplyr::ungroup()%>%
-       tidyr::pivot_longer(cols = comb, values_to = "rate", names_to = "model")
+  {
+    mortfor <- prediction%>%dplyr::left_join(weight$weights)%>%dplyr::mutate(forrates = log(rate)*model.weights)%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(comb = exp(sum(forrates)))%>%dplyr::ungroup()%>%
+      tidyr::pivot_longer(cols = comb, values_to = "rate", names_to = "model")
 
-     allForecast_one <- dplyr::bind_rows(prediction,  mortfor)
+    allForecast_one <- dplyr::bind_rows(prediction,  mortfor)
 
-     # observed rates
+    # observed rates
 
-     obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),cols = 2:(length(data$years)+1),
-                                       names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
+    obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),cols = 2:(length(data$years)+1),
+                                      names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
 
-     allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
+    allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
 
-     # compute errors
+    # compute errors
 
-     mseDF <- allForecast%>%dplyr::mutate(err2 = (log(rate) - log(obsrate))^2)%>%dplyr::group_by(h, model)%>%dplyr::summarise(mse = mean(err2, na.rm = TRUE))%>%dplyr::ungroup()
+    mseDF <- allForecast%>%dplyr::mutate(err2 = (log(rate) - log(obsrate))^2)%>%dplyr::group_by(h, model)%>%dplyr::summarise(mse = mean(err2, na.rm = TRUE))%>%dplyr::ungroup()
 
-     output <-list(comb.rates = na.omit(mortfor), model.rates = prediction, comb.method = weight$comb.method, mse = na.omit(mseDF))
+    output <-list(comb.rates = na.omit(mortfor), model.rates = prediction, comb.method = weight$comb.method, mse = na.omit(mseDF))
 
 
-     class(output) <- "CoMoMo.weight"
+    class(output) <- "CoMoMo.weight"
 
-     return(output)
+    return(output)
 
-   }
+  }
 
- else if (max(weight$weights$h) < h)
+  else if (max(weight$weights$h) < h)
 
- {
-   # updates the weights here when having weights for fewer horizons
+  {
+    # updates the weights here when having weights for fewer horizons
 
-   start.horizon <- max(weight$weights$h)
+    start.horizon <- max(weight$weights$h)
 
-   interval<- h - start.horizon
+    interval<- h - start.horizon
 
-   weightsDF <- (dplyr::bind_rows(lapply(rep(list(as.data.frame(tail(weight$weights, length(models)))), interval), function(x) x%>%dplyr::mutate(model = names(models))))%>%dplyr::mutate(h = rep(1:interval + start.horizon, each=length(models))))[, c("h","model.weights","model")]
+    weightsDF <- (dplyr::bind_rows(lapply(rep(list(as.data.frame(tail(weight$weights, length(models)))), interval), function(x) x%>%dplyr::mutate(model = names(models))))%>%dplyr::mutate(h = rep(1:interval + start.horizon, each=length(models))))[, c("h","model.weights","model")]
 
-   weightsDFall <- rbind(weight$weights, weightsDF)
+    weightsDFall <- rbind(weight$weights, weightsDF)
 
-   weight$weights <- weightsDFall
+    weight$weights <- weightsDFall
 
-   mortfor <- prediction%>%dplyr::left_join(weight$weights)%>%dplyr::mutate(forrates = log(rate)*model.weights)%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(comb = exp(sum(forrates)))%>%dplyr::ungroup()%>%
-     tidyr::pivot_longer(cols = comb, values_to = "rate", names_to = "model")
+    mortfor <- prediction%>%dplyr::left_join(weight$weights)%>%dplyr::mutate(forrates = log(rate)*model.weights)%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(comb = exp(sum(forrates)))%>%dplyr::ungroup()%>%
+      tidyr::pivot_longer(cols = comb, values_to = "rate", names_to = "model")
 
-   allForecast_one <- dplyr::bind_rows(prediction, mortfor)
+    allForecast_one <- dplyr::bind_rows(prediction, mortfor)
 
-   # observed rates
+    # observed rates
 
-   obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),cols = 2:(length(data$years)+1),
-                                     names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
+    obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),cols = 2:(length(data$years)+1),
+                                      names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
 
-   allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
+    allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
 
-   # compute errors
+    # compute errors
 
-   mseDF <- allForecast%>%dplyr::mutate(err2 = (log(rate) - log(obsrate))^2)%>%dplyr::group_by(h, model)%>%dplyr::summarise(mse = mean(err2, na.rm = TRUE))%>%dplyr::ungroup()
+    mseDF <- allForecast%>%dplyr::mutate(err2 = (log(rate) - log(obsrate))^2)%>%dplyr::group_by(h, model)%>%dplyr::summarise(mse = mean(err2, na.rm = TRUE))%>%dplyr::ungroup()
 
-   output <-list(comb.rates = na.omit(mortfor), model.rates = prediction, comb.method = weight$comb.method, mse = na.omit(mseDF))
+    output <-list(comb.rates = na.omit(mortfor), model.rates = prediction, comb.method = weight$comb.method, mse = na.omit(mseDF))
 
-   class(output) <- "CoMoMo.weight"
+    class(output) <- "CoMoMo.weight"
 
-   return(output)
+    return(output)
 
- }
+  }
 
-   else if (max(weight$weights$h) > h)
+  else if (max(weight$weights$h) > h)
 
-   {
-     # updates the weights when predicting shorter horizon than the horizon
+  {
+    # updates the weights when predicting shorter horizon than the horizon
 
-     # for the weights
+    # for the weights
 
-     weight$weights <- head(weight$weights, length(models)*h)
+    weight$weights <- head(weight$weights, length(models)*h)
 
-     mortfor <- prediction%>%dplyr::left_join(weight$weights)%>%dplyr::mutate(forrates = log(rate)*model.weights)%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(comb = exp(sum(forrates)))%>%dplyr::ungroup()%>%
-       tidyr::pivot_longer(cols = comb, values_to = "rate", names_to = "model")
+    mortfor <- prediction%>%dplyr::left_join(weight$weights)%>%dplyr::mutate(forrates = log(rate)*model.weights)%>%dplyr::group_by(ages, year, h)%>%dplyr::summarise(comb = exp(sum(forrates)))%>%dplyr::ungroup()%>%
+      tidyr::pivot_longer(cols = comb, values_to = "rate", names_to = "model")
 
-     allForecast_one <- dplyr::bind_rows(prediction,  mortfor)
+    allForecast_one <- dplyr::bind_rows(prediction,  mortfor)
 
-     # observed rates
+    # observed rates
 
-     obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),cols = 2:(length(data$years)+1),
-                                       names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
+    obsRatesDF <- tidyr::pivot_longer(dplyr::bind_cols(ages = data$ages, as.data.frame(data$Dxt/data$Ext)),cols = 2:(length(data$years)+1),
+                                      names_to = "year", values_to = "obsrate")%>%dplyr::mutate(year = as.numeric(year))
 
-     allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
+    allForecast <- dplyr::left_join(allForecast_one, obsRatesDF)
 
-     # compute errors
+    # compute errors
 
-     mseDF <- allForecast%>%dplyr::mutate(err2 = (log(rate) - log(obsrate))^2)%>%dplyr::group_by(h, model)%>%dplyr::summarise(mse = mean(err2, na.rm = TRUE))%>%dplyr::ungroup()
+    mseDF <- allForecast%>%dplyr::mutate(err2 = (log(rate) - log(obsrate))^2)%>%dplyr::group_by(h, model)%>%dplyr::summarise(mse = mean(err2, na.rm = TRUE))%>%dplyr::ungroup()
 
-     output <-list(comb.rates = na.omit(mortfor), model.rates = prediction, comb.method = weight$comb.method, mse = na.omit(mseDF))
+    output <-list(comb.rates = na.omit(mortfor), model.rates = prediction, comb.method = weight$comb.method, mse = na.omit(mseDF))
 
-     class(output) <- "CoMoMo.weight"
+    class(output) <- "CoMoMo.weight"
 
-     return(output)
+    return(output)
 
-   }
+  }
 }
 
 
