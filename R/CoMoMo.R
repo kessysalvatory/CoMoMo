b6792df0378a168e8f66c398efb1aa145b93226d
Salvatory Kessy <51252281+kessysalvatory@users.noreply.github.com>
2021-06-19 22:08:54 +1000

    Update CoMoMo.R

--- R/CoMoMo.R
+++ R/CoMoMo.R
@@ -3,20 +3,21 @@
 #'
 #' @return Returns an object of class \code{CoMoMo} with the following components:
 #'
-#' \item{Comb}{Returns the combinated forecasts for different horizon.}
+#' \item{Comb}{Returns the fitted GAPC mortality models.}
+#' \item{Comb}{Returns the combination weights.}
+#' \item{Comb}{Returns the combination method.}
+#' \item{Comb}{Returns the meta-learner if the combination method is the stacked regression ensemble.}
 #'
 #'  @examples
-#'
+#'  Dont run 
 #'  modelFits <- fitCoMoMo(models, data = DataStMoMo, ages.fit = agesFit, years.fit = yearsFit)
-#'
-#'  modcom <- CoMoMo(modelFits, weight = stack_nnls_weight)
-#'
+#'  modcom <- CoMoMo(modelFits, weight = stack_nnls_weight) 
 #'
 #' @export
 
 CoMoMo  <- function(object, weight = NULL)
 
-{
+{ # when model combination is simple model averaging. 
 
   if(is.null(weight))
 
@@ -28,6 +29,7 @@ CoMoMo  <- function(object, weight = NULL)
     return(output)
 
   }
+   # Other model combinations 
 
   else {
 
